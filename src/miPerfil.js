7ed89f54536e733139310e8679d8928a771111df
MaleBurs <maleburs@gmail.com>
2020-05-30 12:49:58 -0300

    FirstCommit
    first steps

--- src/miPerfil.js
+++ src/miPerfil.js
@@ -11,7 +11,7 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import './shared-styles.js';
 
-class MyView2 extends PolymerElement {
+class MiPerfil extends PolymerElement {
   static get template() {
     return html`
       <style include="shared-styles">
@@ -23,13 +23,10 @@ class MyView2 extends PolymerElement {
       </style>
 
       <div class="card">
-        <div class="circle">2</div>
-        <h1>View Two</h1>
-        <p>Ea duis bonorum nec, falli paulo aliquid ei eum.</p>
-        <p>Id nam odio natum malorum, tibique copiosae expetenda mel ea.Detracto suavitate repudiandae no eum. Id adhuc minim soluta nam.Id nam odio natum malorum, tibique copiosae expetenda mel ea.</p>
+        <p>TODO MI PERFIL</p>
       </div>
     `;
   }
 }
 
-window.customElements.define('my-view2', MyView2);
+window.customElements.define('mi-perfil', MiPerfil);
