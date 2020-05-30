5c01571af2ec1547cac4327eb498a6a42a32cd6d
igniparra <igniparra@gmail.com>
2020-05-30 17:26:16 -0300

    Agregado Registrar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 var mysql = require('mysql');
+var util = require('util');
 var TwinBcrypt = require('twin-bcrypt');
 const app = express();
 const port = 3000;
@@ -22,39 +23,18 @@ var db  = mysql.createPool({
   database        : '3deseos'
 });
 
-const passPlain = "123456";
-var hash = "$2y$10$fMLm6NiTOPx3XLHJ3m4inuBTTuAVWtCqwJCoATA3ZABhWY3RJFDK6";
-const saltRounds = 10;
-bcrypt.hash(passPlain, saltRounds, function(err, hash) {
-    console.log(hash);
-});
-
-var twinhash = TwinBcrypt.hashSync("123456",10);
-
-
-TwinBcrypt.compare("123456", hash, function(result) {
-    console.log("twinCompare: "+result);
-});
-
-var rounds=bcrypt.getRounds(hash);
-console.log(rounds);
-
-bcrypt.compare(passPlain, hash, function(err, result) {
-    console.log("compare: "+result);
-});
 
 app.post('/api/login', (req, res) =>{
   var username = req.body.request.username;
-  var password = TwinBcrypt.hashSync(req.body.request.password,10);
+  var password = req.body.request.password;
   var response = false;
   //const fingerprint =  req.body.request.fingerprint;
 
-  db.query('SELECT name from users where email=? and password=?',[username,password], function (error, results, fields) {
+  db.query('SELECT * from users where email=?',[username], function (error, results, fields) {
     if (error) throw error;
     if (results[0]==undefined) {
       response = false;
-    }
-    else {
+    } else if (TwinBcrypt.compareSync(password, results[0].password)) {
       response = true;
     }
     res.send(response);
@@ -63,11 +43,12 @@ app.post('/api/login', (req, res) =>{
 
 app.post('/api/register', (req, res) =>{
   const username = req.body.request.username;
-  const password = req.body.request.password;
-  //const fingerprint =  req.body.request.fingerprint;
-
-  var loginResult = true;
-  res.send(loginResult);
+  const password = TwinBcrypt.hashSync(req.body.request.password);
+  db.query('INSERT into users (email,password) values (?,?)',[username,password], function (error, results, fields) {
+    console.log(error);
+    if (error) throw error;
+    res.send(true);
+  });
 })
 
 app.listen(port, () => console.log(`3Deseos Donor Backend (3DDB) is running on port ${port}!`))
