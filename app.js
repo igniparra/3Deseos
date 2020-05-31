95f85a8bfbcfc9133292af38adc81c32611b932d
igniparra <igniparra@gmail.com>
2020-05-31 11:31:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -89,13 +89,42 @@ app.post('/api/getONG', (req, res) =>{
   });
 })
 
+Date.prototype.getWeek = function (dowOffset) {
+/*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
+    dowOffset = typeof(dowOffset) == 'int' ? dowOffset : 0; //default dowOffset to zero
+    var newYear = new Date(this.getFullYear(),0,1);
+    var day = newYear.getDay() - dowOffset; //the day of week the year begins on
+    day = (day >= 0 ? day : day + 7);
+    var daynum = Math.floor((this.getTime() - newYear.getTime() -
+    (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
+    var weeknum;
+    //if the year starts before the middle of a week
+    if(day < 4) {
+        weeknum = Math.floor((daynum+day-1)/7) + 1;
+        if(weeknum > 52) {
+            nYear = new Date(this.getFullYear() + 1,0,1);
+            nday = nYear.getDay() - dowOffset;
+            nday = nday >= 0 ? nday : nday + 7;
+            /*if the next year starts before the middle of
+              the week, it is week #1 of that year*/
+            weeknum = nday < 4 ? 1 : 53;
+        }
+    }
+    else {
+        weeknum = Math.floor((daynum+day-1)/7);
+    }
+    return weeknum;
+};
+
 app.post('/api/getInterests', (req, res) =>{ //TODO
   const ong = req.body.request.ong;
   const startDate = req.body.request.startDate;
   var endDate = new Date();
   endDate.setDate(startDate.getDate() + 30);
+  var startWeek=startDate.getWeek();
+  var endWeek=endDate.getWeek();
   var interests = [];
-  db.query('SELECT * from ..... into users (email,password) values (?,?)',[username,password], function (error, results, fields) {
+  db.query('SELECT * from ..... into users (email,password) values (?,?)',[ong], function (error, results, fields) {
     console.log(error);
     if (error) throw error;
     res.send(true);
